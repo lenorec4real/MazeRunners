864bb8befc7deea06000e3f60ddef651605f0c5c
Lenore Chen <lenore@profitablesoftware.com>
2021-12-22 16:30:37 -0800

    delete build

--- build/ext/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lenorechen/Desktop/CPSC427/team10/ext/yaml-cpp/util
-# Build directory: /Users/lenorechen/Desktop/CPSC427/team10/build/ext/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
