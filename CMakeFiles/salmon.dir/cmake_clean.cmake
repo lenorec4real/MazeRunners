1be44ae212833aecbe27aa25714c7b46cc5a05c5
Lenore Chen <lenore@profitablesoftware.com>
2021-12-22 16:09:24 -0800

    mvp project

---
+++ CMakeFiles/salmon.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/salmon.dir/src/ai_system.cpp.o"
+  "CMakeFiles/salmon.dir/src/ai_system.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/common.cpp.o"
+  "CMakeFiles/salmon.dir/src/common.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/components.cpp.o"
+  "CMakeFiles/salmon.dir/src/components.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/main.cpp.o"
+  "CMakeFiles/salmon.dir/src/main.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/menu.c.o"
+  "CMakeFiles/salmon.dir/src/menu.c.o.d"
+  "CMakeFiles/salmon.dir/src/menu_level_reader.cpp.o"
+  "CMakeFiles/salmon.dir/src/menu_level_reader.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/physics_system.cpp.o"
+  "CMakeFiles/salmon.dir/src/physics_system.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/render_system.cpp.o"
+  "CMakeFiles/salmon.dir/src/render_system.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/render_system_init.cpp.o"
+  "CMakeFiles/salmon.dir/src/render_system_init.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/world_init.cpp.o"
+  "CMakeFiles/salmon.dir/src/world_init.cpp.o.d"
+  "CMakeFiles/salmon.dir/src/world_system.cpp.o"
+  "CMakeFiles/salmon.dir/src/world_system.cpp.o.d"
+  "salmon"
+  "salmon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/salmon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/ext/yaml-cpp/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/lenorechen/Desktop/CPSC427/team10/ext/yaml-cpp
+# Build directory: /Users/lenorechen/Desktop/CPSC427/team10/build/ext/yaml-cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("util")
